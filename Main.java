619d74e489ed6644053aa74439b526c36464b15b
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-03 12:50:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        
+        String[] words = {"the", "red", "fox", "the", "red"};
+        WordPairList wpList = new WordPairList(words);
+        System.out.println(wpList.numMatches());
+
+        String[] wordNums = {"one", "two", "three"};
+        WordPairList exampleOne = new WordPairList(wordNums);
+        System.out.println(exampleOne.numMatches());
+
+        String[] words2 = {"apple", "banana", "cherry", "date"};
+        WordPairList exampleTwo = new WordPairList(words2);
+        System.out.println(exampleTwo.numMatches());
+
+        String[] moreWords = {"the", "red", "fox", "the", "red"};
+        WordPairList exampleThree = new WordPairList(moreWords);
+        System.out.println(exampleThree.numMatches());
     }
 }
