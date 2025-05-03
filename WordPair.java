733c87f421dd917029bfd2f9f5842d543a78531c
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-03 12:58:04 -0400

    Update WordPair.java

--- WordPair.java
+++ WordPair.java
@@ -1,32 +1,26 @@
-import java.util.ArrayList;
+public class WordPair {
+  
+    private String first;
 
-public class WordPairList {
-
-    private ArrayList<WordPair> allPairs;
+    private String second;
 
 
-    public WordPairList(String[] words) {
-        allPairs = new ArrayList<WordPair>();
-        for (int i = 0; i < words.length - 1; i++) {
-            for (int j = i + 1; j < words.length; j++) {
-                WordPair w = new WordPair(words[i], words[j]);
-                allPairs.add(w);
-            }
-        }
-    }
-
-    public ArrayList<WordPair> getAllPairs() {
-        return allPairs;
+    public WordPair(String first, String second) {
+        this.first = first;
+        this.second = second;
     }
 
 
-    public int numMatches() {
-        int matches = 0;
-        for (WordPair w : allPairs) {
-            if (w.getFirst().equals(w.getSecond())) {
-                matches++;
-            }
-        }
-        return matches;
+    public String getFirst() {
+        return first;
+    }
+
+   
+    public String getSecond() {
+        return second;
+    }
+
+    public String toString() {
+        return "(" + first + ", " + second + ")";
     }
 }
